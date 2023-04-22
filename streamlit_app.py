dad777406e9fe1bbe9c37b4d9367e2a3ce51e323
Rahul Bhatt <rbhatt.27@gmail.com>
2023-04-22 17:14:18 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ streamlit.header('Breakfast Favorites');
 streamlit.text('🥣 and Blueberry Oatmeal');
 streamlit.text('🥗 Spinach & Rocket Smoothie');
 streamlit.text('🐔 Hard-Boiled Free-Range Egg');
-streamlit.text('🥑🍞 Avocado Toast);
+streamlit.text('🥑🍞 Avocado Toast');
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
